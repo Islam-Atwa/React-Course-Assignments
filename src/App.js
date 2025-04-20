97087182398a9bd31d0ac5524fbdc2fdeab08e19
Islam Atwa <islamatwa429@gmail.com.com>
2025-04-21 00:28:34 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Post from './Post';
-import SideMenu from './SideMenu';
-// import TagButton from './TagButton';
+import MyButton from './MyButton';
 function App() {
   return (
     <div className="App">
-      <Header/>
-      
-      {/* Post Container */}
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div
-          style={{ width: "60%", display: "flex", justifyContent: "center" }}
+      <header className="App-header">
+        <img src={logo} className="App-header App-logo" alt="logo"/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          {/* == POSTS CONTAINER == */}
-          <div style={{ width: "70%" }}>
-            <Post postName="What is React ? " postBody="React is a JavaScript library. used to bulid a UI components" />
-            <Post postName="Why We Need React " postBody="to build fast, interactive user interfaces efficiently using reusable components, a virtual DOM for performance, and a unidirectional data flow for predictable state management." />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-          </div>
-
-          <div style={{ width: "30%", marginTop: "25px" }}>
-            <SideMenu />
-          </div>
-        </div>
-      </div>
+          Learn React
+        </a>
+        <MyButton/>
+      </header>
     </div>
   );
 }
