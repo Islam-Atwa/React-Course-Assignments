269c1d0190b94c2d3cddf1575e2166a63d278263
Islam Atwa <islamatwa429@gmail.com.com>
2025-05-11 03:00:21 +0300

    InsertingIntoAnArray

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 // import UpdatingObjectState from './components/UpdatingObjectState.js';
 // import ArraysInState from './Array/ArraysInState.js';
-import TransformingAnArray from './Array/Transforming_an_array.js';
-
+// import TransformingAnArray from './Array/Transforming_an_array.js';
+import InsertingIntoAnArray from './Array/InsertingIntoAnArray.js';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <ArraysInState/> */}
-        <TransformingAnArray/>
+        <InsertingIntoAnArray/>
+        {/* <TransformingAnArray/> */}
       </header>
     </div>
   );
