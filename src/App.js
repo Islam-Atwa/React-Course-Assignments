7803ed9970ed80d1d1a4981b340091840b5c0792
Islam Atwa <islamatwa429@gmail.com.com>
2025-05-18 22:58:21 +0300

    challange soultion Update an item in the shopping cart

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 // import UpdatingObjectState from './components/UpdatingObjectState.js';
 // import ArraysInState from './Array/ArraysInState.js';
-// import TransformingAnArray from './Array/Transforming_an_array.js';
-import InsertingIntoAnArray from './Array/InsertingIntoAnArray.js';
+
+import Updateitem from './Array/UpdateItem.js';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <ArraysInState/> */}
-        <InsertingIntoAnArray/>
-        {/* <TransformingAnArray/> */}
+        <Updateitem/>
       </header>
     </div>
   );
